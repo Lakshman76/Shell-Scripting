1cb0562bdab622918ace6fb10c1138bd0c05a734
Lakshman76 <lakshmankumar2603@gmail.com>
2025-02-27 20:52:30 +0530

    command line arguments

---
+++ cmdLineArguments.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Command Line Arguments
+echo "#####################################"
+echo
+echo "COMMAND LINE ARGUMENT"
+echo
+echo "#####################################"
+
+echo "value of 0 is: "
+echo $0
+echo
+
+echo "value of 1 is: "
+echo $1
+echo
+
+echo "value of 2 is: "
+echo $2
+echo
+
+echo "value of 3 is: "$3
+echo
