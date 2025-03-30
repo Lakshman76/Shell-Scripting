a5a9fb0e381a78b3c89278135f6e435192e6668e
Lakshman76 <lakshmankumar2603@gmail.com>
2025-03-30 13:16:40 +0530

    for loop in bash

---
+++ forLoop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for language in JAVA .net Python JS C++ PHP Ruby Dart
+do
+  echo 
+  echo "Looping....."
+  sleep 1
+  echo "#########################################"
+  echo "Value of language is $language"
+  date
+  echo
+done
